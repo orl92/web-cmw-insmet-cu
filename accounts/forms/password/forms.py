0adc25d84a1b8bd8fc22cfe524a4bdce8de44537
Yoelvis Mariné Ramos <yoelvismr@gmail.com>
2025-03-11 13:54:38 -0400

    update

--- accounts/forms/password/forms.py
+++ accounts/forms/password/forms.py
@@ -1,7 +1,7 @@
-import os
-
 from django import forms
+from datetime import datetime
 from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
@@ -46,11 +46,30 @@ class AdminPasswordChangeForm(forms.ModelForm):
         return user
 
 class UserPasswordResetForm(PasswordResetForm):
-    def send_mail(self, subject_template_name, email_template_name,
-                  context, from_email, to_email, html_email_template_name=None):
-        from_email = os.getenv('DEFAULT_FROM_EMAIL')
-        super().send_mail(subject_template_name, email_template_name,
-                          context, from_email, to_email, html_email_template_name)
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)  # Extraer la solicitud del formulario
+        super().__init__(*args, **kwargs)
+
+    def send_mail(self, subject_template_name, email_template_name, context, from_email, to_email, html_email_template_name=None):
+        # Construir la URL completa al índice
+        index_url = self.request.build_absolute_uri(reverse('index'))
+        context['index_url'] = index_url  # Pasar la URL al contexto del correo
+
+        # Construir la URL completa para el enlace de restablecimiento
+        reset_url = self.request.build_absolute_uri(
+            reverse('password_reset_confirm', kwargs={
+                'uidb64': context['uid'],  # UID codificado
+                'token': context['token'],  # Token generado
+            })
+        )
+        context['reset_url'] = reset_url  # Pasar la URL al contexto del correo
+
+        # Agregar el año actual al contexto
+        context['current_year'] = datetime.now().year
+
+        # Llamar al método base para enviar el correo
+        html_email_template_name = 'pages/accounts/emails/password_reset_email.html'  # Template HTML personalizado
+        super().send_mail(subject_template_name, email_template_name, context, from_email, to_email, html_email_template_name)
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
